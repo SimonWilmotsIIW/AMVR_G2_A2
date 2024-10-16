bfc4d089d1f06d34d129c2db7f24e3b8fe69aa2d
W.O.M.P. <56969978+CherryMellonTree@users.noreply.github.com>
2024-10-16 10:02:12 +0100

    start scoremanager

---
+++ ShootTheTarget/Assets/ScoreManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text scoreText; // The UI Text element to display the score
+    private int currentScore = 0;
+
+    void Awake()
+    {
+        // Singleton pattern to ensure there is only one ScoreManager
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        currentScore += scoreToAdd;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + currentScore.ToString();
+    }
+}
