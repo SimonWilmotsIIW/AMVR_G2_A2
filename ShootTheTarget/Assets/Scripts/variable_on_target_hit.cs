0ada36fe4dd7596c4a286a35bc303a006cfa9d81
W.O.M.P. <56969978+CherryMellonTree@users.noreply.github.com>
2024-10-16 19:41:55 +0100

    added scoring & colliders to more elements

--- ShootTheTarget/Assets/Scripts/variable_on_target_hit.cs
+++ ShootTheTarget/Assets/Scripts/variable_on_target_hit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class on_target_hit : MonoBehaviour
+public class variable_on_target_hit : MonoBehaviour
 {
     public int score;
     // Start is called before the first frame update
